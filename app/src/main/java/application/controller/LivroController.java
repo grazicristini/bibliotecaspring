df4db62aa6a896d26cbdc807eb6d44b854aa6dc0
grazicristini <grazielicristhyny2002@gmail.com>
2023-05-24 17:29:09 +0000

    salvar

--- app/src/main/java/application/controller/LivroController.java
+++ app/src/main/java/application/controller/LivroController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import application.model.Livro;
 import application.model.LivroRepository;
 
 @Controller
@@ -24,4 +27,12 @@ public class LivroController {
     public String insert() {
         return "/WEB-INF/livro/insert.jsp";
     }
-}
+
+    @RequestMapping(value = "/insert", method = RequestMethod.POST)
+    public String insert(@RequestParam("titulo") String titulo) {
+        Livro livro = new Livro();
+        livro.setTitulo(titulo);
+        livroRepo.save(livro);
+        return"redirect:/livro/list";
+    }
+ }
